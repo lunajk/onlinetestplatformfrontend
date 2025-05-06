6a4ba25388ea56061334365058184f0a80d725cf
lunajk <vigneshwarisakthivel8@gmail.com>
2025-05-06 10:58:46 +0530

    backend

--- blog/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class BlogConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blog'
-    def ready(self):
-        # Import and connect the signals
-        import blog.signals
